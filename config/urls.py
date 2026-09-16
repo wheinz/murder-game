fa911ace7511b9145670e012f1002efe14a17e1b
wheinz <thomaswever@gmail.com>
2026-09-16 15:42:26 +0300

    fix: serve uploaded media through Django when not behind the bundled Caddy

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,3 +14,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # No separate web server for uploads when deployed behind a shared proxy.
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
